58db6678a45d9f677d3ce29f15eab8d50fd5812d
moaz saeed <moaz.saeed@intigral.net>
2022-04-22 16:34:01 +0400

    Initial Commit

---
+++ NyTimesNewsAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NyTimesNewsAssignment
+//
+//  Created by Moaz Saeed (c) on 22/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
