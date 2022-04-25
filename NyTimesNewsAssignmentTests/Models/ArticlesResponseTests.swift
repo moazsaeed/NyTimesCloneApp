69f41f53a64be52ecfd00fafeb6c8c9c4380b1f1
moaz saeed <moaz.saeed@intigral.net>
2022-04-25 17:28:12 +0400

    added model testfile

--- NyTimesNewsAssignmentTests/Models/ArticlesResponseTests.swift
+++ NyTimesNewsAssignmentTests/Models/ArticlesResponseTests.swift
@@ -1,14 +1,13 @@
 //
-//  NyTimesNewsAssignmentTests.swift
+//  ArticlesResponseTests.swift
 //  NyTimesNewsAssignmentTests
 //
-//  Created by Moaz Saeed (c) on 22/04/2022.
+//  Created by Moaz Saeed (c) on 25/04/2022.
 //
 
 import XCTest
-@testable import NyTimesNewsAssignment
 
-class NyTimesNewsAssignmentTests: XCTestCase {
+class ArticlesResponseTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
